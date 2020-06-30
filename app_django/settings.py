0df690e54761a322ff66198308bed7c72e6db439
ericalbertodasilva <ercker@gmail.com>
2020-06-30 19:58:38 -0300

    feat: db postgresql configured

--- app_django/settings.py
+++ app_django/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'app_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'course_django',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
